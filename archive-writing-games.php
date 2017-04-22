3cce548f9eb348d35c0a67463b24d2c6ee980a10
r0ma <wbfx@ya.ru>
2017-04-23 03:13:19 +0500

    Исправлена синтаксическая ошибка, при которой не рендилось ничего.

--- archive-writing-games.php
+++ archive-writing-games.php
@@ -1,7 +1,7 @@
 <?php
-// echo $current_url = add_query_arg($wp->query_string, '', home_url($wp->request));
 
-if ( $_SERVER[' REQUEST_METHOD' ] == GET ) {
+
+if ( $_SERVER['REQUEST_METHOD' ] === 'GET' ) {
 
 	if ( get_end_point() == FALSE ) {
 
@@ -12,17 +12,16 @@ if ( $_SERVER[' REQUEST_METHOD' ] == GET ) {
 }
 
 
-if ( $_SERVER[' REQUEST_METHOD' ] == POST ) {
+if ( $_SERVER['REQUEST_METHOD' ] == 'POST' ) {
 	// $current_url = get_current_url();
 	// $game_id = get_id_score( $current_url );
 	wg_debuger( insert_stat_table( $_POST ) );
+
 }
 
 
-if ( $_SERVER[ 'REQUEST_METHOD' ] == PUT ) {
+if ( $_SERVER[ 'REQUEST_METHOD' ] == 'PUT' ) {
 
-	// parse_str(file_get_contents('php://input'), $requestData);
 	wg_debuger( update_game_table() );
-	// wg_debuger($requestData);
 
 }
