c029e6c2245c15a6f4900b568eea60ba9285240c
r0ma <wbfx@ya.ru>
2017-04-23 01:45:52 +0500

    Вывод обработки данных методом POST Пустная заготовка для метода PUT

--- archive-writing-games.php
+++ archive-writing-games.php
@@ -1,4 +1,5 @@
 <?php
+// echo $current_url = add_query_arg($wp->query_string, '', home_url($wp->request));
 
 if ( $_SERVER['REQUEST_METHOD'] == GET ) {
 
@@ -11,52 +12,14 @@ if ( $_SERVER['REQUEST_METHOD'] == GET ) {
 }
 
 
-
 if ( $_SERVER['REQUEST_METHOD'] == POST ) {
-	$id = get_end_point();
-
-	wg_debuger( $_POST );
-	$game_id = $_POST['game_id'];
-	global $wpdb;
-    $info = $wpdb->insert( 'wp_game_statistics', ['game_id' => $game_id], ['%d'] );
-    var_dump( $info );
-	// wg_debuger( $id );
+	// $current_url = get_current_url();
+	// $game_id = get_id_score( $current_url );
+	wg_debuger( insert_in_stat_db( $_POST ) );
 }
 
 
+if ( $_SERVER['REQUEST_METHOD'] == PUT ) {
 
 
-$path = plugin_dir_url('writing-games/sent_post.php');
-$url = $path . 'sent_post.php';
-$args = array(
-	'method' => 'POST',
-	'body' => array(
-		'game_id' => '99',
-		'game_type' => 'logic',
-		'user_name' => 'Leo',
-		'score' => '1.2',
-		'score_desc' => 'Description Leo game',
-	),
-);
-$response = wp_remote_post( $url, $args );
-
-// проверка ошибки
-if ( is_wp_error( $response ) ) {
-   $error_message = $response->get_error_message();
-   echo "Что-то пошло не так: $error_message";
-} else {
-   $bodys = $response['body'];
-   // echo 'Ответ: <pre>';
-   // print_r( $bodys );
-   // echo '</pre>';
-   // extract($bodys, EXTR_PREFIX_SAME);
-   // echo $game_id;
-   // global $wpdb;
-   // $table_stat = TABLEGAMESTAT;
-   // $info = $wpdb->get_row( "SELECT * FROM $table_stat WHERE `game_id` = $id ORDER BY `score` DESC", ARRAY_A );
-   // return json_encode( $info );
 }
-
-
-
-?>
