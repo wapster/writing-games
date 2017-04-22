fe326af6ee19e7528e6720178763f4cf7d602130
r0ma <wbfx@ya.ru>
2017-04-23 01:44:55 +0500

    Небольшие дополнения: более "правдоподобный массив с данными", url

--- root.php
+++ root.php
@@ -5,12 +5,34 @@ ini_set( 'display_startup_errors', 1 );
 
 set_time_limit( 0 );
 
-require ( $_SERVER['DOCUMENT_ROOT'].'/writing-games/wp-load.php' );
+// require ( $_SERVER['DOCUMENT_ROOT'].'/writing-games/wp-load.php' );
 
 
-$ch = curl_init();
-$data = array( 'game_id' => 100 );
-curl_setopt( $ch, CURLOPT_URL, 'http://localhost/writing-games/writing-games/23/score/' );
+// Получить id для страниц формата site.com/writing-games/12/score/
+function get_id( $url ) {
+	$i = explode( 'score', $url );
+	$i = explode( '/', $i[0] );
+	$i = array_diff( $i, array('') );
+	$id = array_pop( $i );
+    return $id;
+}
+
+
+$url = 'http://localhost/writing-games/writing-games/18/score/';
+
+// получаем id из url
+$id = get_id( $url );
+
+$data = array(
+    'game_id' => $id,
+    'game_type' => 'writing-games',
+    'user_name' => 'Roman',
+    'score' => '32.9',
+    'score_desc' => 'description of score',
+    );
+
+$ch = curl_init( $url );
+// curl_setopt( $ch, CURLOPT_URL, $url );
 curl_setopt( $ch, CURLOPT_POST, 1 );
 curl_setopt( $ch, CURLOPT_POSTFIELDS, $data );
 // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
